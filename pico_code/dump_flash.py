a77d99d1f7bf01253bdd8b7264b8d0895eb21f33
Daniel Schwendner <officialcodebyter@gmail.com>
2025-03-13 20:33:31 +0100

    Add solution

--- pico_code/dump_flash.py
+++ pico_code/dump_flash.py
@@ -2,11 +2,10 @@ from machine import Pin, SPI, PWM
 import time
 
 # SPI Commands
-# ToDo: Define the SPI commands
-READ_DATA = 
-READ_STATUS = 
-READ_MANUFACTURER_ID = 
-RELEASE_DEEP_POWER_DOWN = 
+READ_DATA = 0x03
+READ_STATUS = 0x05
+READ_MANUFACTURER_ID = 0x90
+RELEASE_DEEP_POWER_DOWN = 0xAB
 
 # SPI Pins
 SPI_SCK = 2
@@ -17,7 +16,7 @@ CS_PIN = 1
 cs = Pin(CS_PIN, Pin.OUT)
 
 # ToDo: polarity and phase values differ on some WiFi Routers
-spi = SPI(0, baudrate=1000, polarity=1, phase=1, sck=Pin(SPI_SCK), mosi=Pin(SPI_MOSI), miso=Pin(SPI_MISO))
+spi = SPI(0, baudrate=1000, polarity=0, phase=1, sck=Pin(SPI_SCK), mosi=Pin(SPI_MOSI), miso=Pin(SPI_MISO))
 
 
 def select_chip():
@@ -31,11 +30,13 @@ def deselect_chip():
 
 def read_data(address, length, cmd=READ_DATA):
     select_chip()
-    # ToDo spi write_readinto
+    data = bytearray(length)
+    spi.write_readinto(bytes([READ_DATA]) + address.to_bytes(3, 'big') + bytes(length-4), data)
+    print(data)
     deselect_chip()
     return data
 
 
-address = # ToDo
+address = 0x00d120
 data = read_data(address, 16)
 print("Dumped data:", [hex(x) for x in data])
